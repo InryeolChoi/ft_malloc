0c51a490de35f21eeb50aeea47830269814daf0a
InryeolChoi <dlsfuf0316@gmail.com>
2026-06-10 00:45:50 +0900

    Start malloc box creation

--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -7,55 +7,56 @@
 
 # define TINY_MAX 128
 # define SMALL_MAX 1024
+# define TAG_MAGIC ((size_t)0xC0FFEE)
 
 typedef enum e_zone_type
 {
 	ZONE_TINY,
 	ZONE_SMALL,
 	ZONE_LARGE
-}						t_zone_type;
+}	t_zone_type;
 
 typedef struct s_box
 {
-	void				*start;
-	size_t				size;
-	t_zone_type			type;
-	struct s_tag		*first_tag;
-	struct s_box		*next_box;
-}						t_box;
+	void			*start;
+	size_t			size;
+	t_zone_type		type;
+	struct s_tag	*first_tag;
+	struct s_box	*next_box;
+}	t_box;
 
 typedef struct s_tag
 {
-	size_t				user_area_size;
-	int					is_free;
-	struct s_tag		*next_tag;
-	struct s_tag		*prev_tag;
-	size_t				magic;
-}						t_tag;
+	size_t			user_area_size;
+	int				is_free;
+	struct s_tag	*next_tag;
+	struct s_tag	*prev_tag;
+	size_t			magic;
+}	t_tag;
 
 typedef struct s_malloc_state
 {
-	t_box				*tiny_boxes;
-	t_box				*small_boxes;
-	t_box				*large_boxes;
-}						t_malloc_state;
+	t_box	*tiny_boxes;
+	t_box	*small_boxes;
+	t_box	*large_boxes;
+}	t_malloc_state;
 
 extern t_malloc_state	g_malloc;
 
-size_t					get_basic_page_size(void);
-t_zone_type				get_zone_type(size_t size);
-t_box					**get_box_list(t_zone_type type);
-t_box					*find_box_pool(void *ptr);
-t_box					*find_box_list(t_box *box_list, void *ptr);
-t_tag					*find_tag_in_box(t_box *box, void *ptr);
-int						is_ptr_in_box(t_box *box, void *ptr);
-size_t					get_box_size(t_zone_type type, size_t user_size, size_t page);
-size_t					will_add_overflow(size_t num1, size_t num2);
-size_t					will_multi_overflow(size_t num1, size_t num2);
+size_t			get_basic_page_size(void);
+t_zone_type		get_zone_type(size_t size);
+t_box			**get_box_list(t_zone_type type);
+t_box			*find_box_pool(void *ptr);
+t_box			*find_box_list(t_box *box_list, void *ptr);
+t_tag			*find_tag_in_box(t_box *box, void *ptr);
+int				is_ptr_in_box(t_box *box, void *ptr);
+size_t			get_box_size(t_zone_type type, size_t user_size, size_t page);
+size_t			will_add_overflow(size_t num1, size_t num2);
+size_t			will_multi_overflow(size_t num1, size_t num2);
 
-void					*malloc(size_t size);
-void					free(void *ptr);
-void					*realloc(void *ptr, size_t size);
-void					show_alloc_mem(void);
+void	*malloc(size_t size);
+void	free(void *ptr);
+void	*realloc(void *ptr, size_t size);
+void	show_alloc_mem(void);
 
 #endif
--- src/malloc.c
+++ src/malloc.c
@@ -4,7 +4,54 @@ t_malloc_state	g_malloc = {0};
 
 void	*malloc(size_t size)
 {
-	(void)size;
+	size_t		boxsize;
+	t_zone_type	type;
+	t_box		*box;
+	t_box		**box_list;
+
+	/*
+	 * malloc 설계 흐름:
+	 *
+	 * 1. 먼저 size == 0 정책을 정한다.
+	 *    malloc(0), realloc(ptr, 0)의 동작이 서로 어긋나지 않게 한다.
+	 *
+	 * 2. 요청 size로 zone type을 정한다.
+	 *    type = get_zone_type(size)
+	 *
+	 * 3. type에 맞는 box list를 고른다.
+	 *    box_list = get_box_list(type)
+	 *
+	 * 4. 새 box를 만들기 전에 기존 box들을 먼저 뒤진다.
+	 *    아직 필요한 helper:
+	 *    - find_free_tag_in_box(box, size)
+	 *    - find_free_tag_in_boxes(*box_list, size)
+	 *
+	 *    malloc에 쓸 수 있는 tag 조건:
+	 *    - tag->magic == TAG_MAGIC
+	 *    - tag->is_free == 1
+	 *    - tag->user_area_size >= size
+	 *
+	 * 5. 재사용 가능한 free tag가 있으면 그 tag를 할당 상태로 바꾼다.
+	 *    나중에는 남은 공간이 충분할 때 tag를 split해야 한다.
+	 *
+	 * 6. 재사용 가능한 tag가 없을 때만 새 box를 만든다.
+	 *    create_box(type, boxsize)는 mmap으로 메모리를 받고
+	 *    t_box와 첫 번째 free t_tag를 초기화해야 한다.
+	 *
+	 * 7. 새 box를 *box_list에 연결한다.
+	 *
+	 * 8. 새 box의 첫 free tag에서 할당한다.
+	 *
+	 * 9. malloc의 반환값은 box 주소가 아니라 tag_to_user(tag)다.
+	 *
+	 * 현재 아래 코드는 create_box로 너무 빨리 간다.
+	 * 다음에는 기존 box에서 재사용 가능한 free tag를 찾는 함수부터 만든다.
+	 */
+	type = get_zone_type(size);
+	boxsize = get_box_size(type, size, get_basic_page_size());
+	box = create_box(type, boxsize);
+	box_list = get_box_list(type);
+
 	return NULL;
 }
 
@@ -30,4 +77,27 @@ size_t	get_box_size(t_zone_type type, size_t user_size, size_t page)
 	if ((boxsize = will_multi_overflow(page, boxsize / page + 1)) == 0)
 		return 0;
 	return boxsize;
+}
+
+void	*create_box(t_zone_type tyoe, size_t boxsize)
+{
+	void	*mem;
+	t_box	*box;
+	t_tag	*tag;
+
+	mem = mmap(NULL, boxsize, PROT_READ | PROT_WRITE,
+		MAP_PRIVATE | MAP_ANON, -1, 0);
+	if (mem == MAP_FAILED)
+		return NULL;
+	box = (t_box *)mem;
+	box->size = boxsize;
+	box->start = mem;
+	box->first_tag = (t_tag *)(box + 1);
+
+	tag = box->first_tag;
+	tag->user_area_size = box->size - sizeof(t_box) - sizeof(t_tag);
+	tag->is_free = 1;
+	tag->next_tag = NULL;
+	tag->prev_tag = NULL;
+	tag->magic = TAG_MAGIC;
 }
